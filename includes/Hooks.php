11c4adc3df192520c7a07b84e9beff1d34ddc200
David Molineus <david.molineus@netzmacht.de>
2020-12-12 22:10:59 +0100

    Refactor custom sidebar handling
     - Use more sub methods
     - Reduce cyclomatic complexity

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace MediaWiki\Extension\ForTrainingCustomSidebar;
 
+use BagOStuff;
 use MediaWiki\MediaWikiServices;
 use ObjectCache;
 use Parser;
@@ -17,7 +18,7 @@ class Hooks
     public static function onParserFirstCallInit(Parser $parser): void
     {
         // We can't count of the tag being read if the page is cached.  So instead of this code performing any task
-        // we leave it here to easily remove the <sidebar> tag read in the SkinBuildSidbar hook
+        // we leave it here to easily remove the <sidebar> tag read in the SkinBuildSidebar hook
         // aka lazy way to clear tag
         $parser->setHook('sidebar', function () {
             return '';
@@ -35,38 +36,15 @@ class Hooks
 
         global $wgEnableSidebarCache, $wgSidebarCacheExpiry;
         global $wgDefaultSideBarText, $wgDefaultSideBarGroupText, $wgDefaultSidebarNSText;
-        global $wgCustomSidebarCurrent;    // #custom4training
 
-        $NewSideBar = false;
-
-        if ($skin->getContext()->canUseWikiPage() && $skin->getContext()->getWikiPage()->getContent()) {
-            $pagetext = $wgParser->preprocess(
-                $skin->getContext()->getWikiPage()->getContent()->serialize(),
-                $skin->getTitle(),
-                ParserOptions::newFromUser($skin->getContext()->getUser())
-            );
-
-            if (preg_match('%\<sidebar\>(.*)\</sidebar\>%isU', $pagetext, $matches)) {
-                $NewSideBar             = $matches[1];
-                $wgCustomSidebarCurrent = $matches[1];
-            } elseif (isset($wgDefaultSideBarText) and $wgDefaultSideBarText !== false) {
-                $NewSideBar = $wgDefaultSideBarText;
-            }
+        $key           = ObjectCache::getLocalClusterInstance()->makeKey('sidebar', $skin->getContext()->getLanguage()->getCode());
+        $NewSideBar    = self::getCustomSidebarName($skin, $wgParser);
+        $cachedSidebar = self::getCachedSidebar($key, $parserMemc);
+        if ($cachedSidebar) {
+            return $cachedSidebar;
         }
-
-        // sidebar cache code
-        $key = ObjectCache::getLocalClusterInstance()->makeKey('sidebar', $skin->getContext()->getLanguage()->getCode());
-
-        if ($wgEnableSidebarCache) {
-            $cachedsidebar = $parserMemc->get($key);
-            if ($cachedsidebar) {
-                return $cachedsidebar;
-            }
-        }
-        // end cache code
 
         $new_bar = self::customSidebarProcess($skin, $NewSideBar, $wgParser);
-
         if ((count($new_bar) === 0) && ($wgDefaultSideBarText !== false)) {
             $new_bar = $bar;
         }
@@ -90,11 +68,47 @@ class Hooks
         }
 
         // sidebar cache code
-        if ($wgEnableSidebarCache) $parserMemc->set($key, $bar, $wgSidebarCacheExpiry);
-
+        if ($wgEnableSidebarCache) {
+            $parserMemc->set($key, $bar, $wgSidebarCacheExpiry);
+        }
         // end sidebar cache code
 
         return true;
+    }
+
+    private static function getCustomSidebarName(Skin $skin, Parser $wgParser): ?string
+    {
+        global $wgDefaultSideBarText;
+        global $wgCustomSidebarCurrent; // #custom4training
+
+        if (!$skin->getContext()->canUseWikiPage() || !$skin->getContext()->getWikiPage()->getContent()) {
+            return null;
+        }
+
+        $pageText = $wgParser->preprocess(
+            $skin->getContext()->getWikiPage()->getContent()->serialize(),
+            $skin->getTitle(),
+            ParserOptions::newFromUser($skin->getContext()->getUser())
+        );
+
+        if (preg_match('%\<sidebar\>(.*)\</sidebar\>%isU', $pageText, $matches)) {
+            $wgCustomSidebarCurrent = $matches[1];
+            return $matches[1];
+        } elseif (isset($wgDefaultSideBarText) and $wgDefaultSideBarText !== false) {
+            return $wgDefaultSideBarText;
+        }
+
+        return null;
+    }
+
+    private static function getCachedSidebar(string $key, BagOStuff $parserMemc)
+    {
+        global $wgEnableSidebarCache;
+        if (!$wgEnableSidebarCache) {
+            return null;
+        }
+
+        return $parserMemc->get($key);
     }
 
     private static function customSidebarProcess($skin, $NewSideBar, Parser $wgParser)
@@ -102,30 +116,26 @@ class Hooks
         $NewSideBar = self::customSidebarPreProcess($skin, $NewSideBar, $wgParser);
 
         // custom loader
-        if ($NewSideBar !== false) {
-            if (strpos(trim($NewSideBar), '*') === 0) {
-                $text = $NewSideBar;
-            } else {
-                $text = $NewSideBar;
-                do {
-                    $oldtext = $text;
-                    if (($titleFromText = Title::newFromText($text))) {
-                        $article = new WikiPage($titleFromText);        // #custom4training sonst Bug
-                        $text    = $article->getContent() ? $article->getContent()->getNativeData() : '';    // #custom4training sonst Bug
-                        $text    = preg_replace('%\<noinclude\s*\>(.*)\</noinclude\s*\>%isU', '', $text);
-                        $text    = self::customSidebarPreProcess($skin, $text, $wgParser);
-                    }
-
-                } while ($text !== $oldtext);
-            }
-
-            $lines = explode("\n", $text);
-        } else {
+        if ($NewSideBar === false) {
             return [];
         }
 
-        $new_bar = [];
+        $text = $NewSideBar;
+        if (strpos(trim($NewSideBar), '*') !== 0) {
+            do {
+                $oldtext = $text;
+                if (($titleFromText = Title::newFromText($text))) {
+                    $article = new WikiPage($titleFromText);        // #custom4training sonst Bug
+                    $text    = $article->getContent() ? $article->getContent()->getNativeData() : '';    // #custom4training sonst Bug
+                    $text    = preg_replace('%\<noinclude\s*\>(.*)\</noinclude\s*\>%isU', '', $text);
+                    $text    = self::customSidebarPreProcess($skin, $text, $wgParser);
+                }
 
+            } while ($text !== $oldtext);
+        }
+
+        $lines   = explode("\n", $text);
+        $newBar  = [];
         $heading = '';
 
         // taken directly from MediaWiki source v1.14.0
@@ -135,60 +145,67 @@ class Hooks
             if (strpos($line, '**') !== 0) {
                 $line    = trim($line, '* ');
                 $heading = $line;
-                if (!array_key_exists($heading, $new_bar)) $new_bar[$heading] = [];
+                if (!array_key_exists($heading, $newBar)) {
+                    $newBar[$heading] = [];
+                }
+                continue;
+            }
+
+            if (strpos($line, '|') === false) {
+                continue;
+            }
+
+            // sanity check
+            $line = array_map('trim', explode('|', trim($line, '* '), 2));
+            $link = wfMessage($line[0])->inContentLanguage()->text();    #custom4training Bug
+            if ($link == '-') {
+                continue;
+            }
+
+            $text = wfMessage($line[1])->text(); #custom4training Bug
+
+            #custom4training Bug
+            if (wfMessage($line[1])->inContentLanguage()->isBlank()) {
+                $text = $line[1];
+            }
+
+            #custom4training Bug
+            if (wfMessage($line[0])->inContentLanguage()->isBlank()) {
+                $link = $line[0];
+            }
+
+            if (preg_match('/^(?:' . wfUrlProtocols() . ')/', $link)) {
+                $href = $link;
             } else {
-                if (strpos($line, '|') !== false) { // sanity check
-                    $line = array_map('trim', explode('|', trim($line, '* '), 2));
-                    $link = wfMessage($line[0])->inContentLanguage()->text();    #custom4training Bug
-                    if ($link == '-')
-                        continue;
-
-                    $text = wfMessage($line[1])->text();    #custom4training Bug
-                    if (wfMessage($line[1])->inContentLanguage()->isBlank())    #custom4training Bug
-                        $text = $line[1];
-                    if (wfMessage($line[0])->inContentLanguage()->isBlank())    #custom4training Bug
-                        $link = $line[0];
-
-                    if (preg_match('/^(?:' . wfUrlProtocols() . ')/', $link)) {
-                        $href = $link;
-                    } else {
-                        $title = Title::newFromText($link);
-                        if ($title) {
-                            $title = $title->fixSpecialName();
-                            $href  = $title->getLocalURL();
-                        } else {
-                            $href = 'INVALID-TITLE';
-                        }
-                    }
-
-                    // #custom4training: Welcher Punkt der Sidebar ist gerade aktiv?
-                    $pos    = strpos($href, 'Special:MyLanguage/');
-                    $active = false;
-                    if (($pos === 1) && ($skin->getTitle() !== null)) {
-                        $linkdest = substr($href, 19);        // das "Special:MyLanguage" am Anfang entfernen
-                        $title    = $skin->getTitle()->getLocalURL();
-                        $active   = (substr($title, 0, strlen($linkdest)) === $linkdest);    // Fängt der Artikel damit an? (auch Unterseiten wie "Prayer/de" müssen berücksichtigt werden)
-                    }
-                    $new_bar[$heading][] = [
-                        'text'   => $text,
-                        'href'   => $href,
-                        'id'     => 'n-' . strtr($line[1], ' ', '-'),
-                        'active' => $active,
-                    ];
+                $title = Title::newFromText($link);
+                if ($title) {
+                    $title = $title->fixSpecialName();
+                    $href  = $title->getLocalURL();
                 } else {
-                    continue;
+                    $href = 'INVALID-TITLE';
                 }
             }
+
+            // #custom4training: Welcher Punkt der Sidebar ist gerade aktiv?
+            $pos    = strpos($href, 'Special:MyLanguage/');
+            $active = false;
+            if (($pos === 1) && ($skin->getTitle() !== null)) {
+                $linkdest = substr($href, 19);        // das "Special:MyLanguage" am Anfang entfernen
+                $title    = $skin->getTitle()->getLocalURL();
+                $active   = (substr($title, 0, strlen($linkdest)) === $linkdest);    // Fängt der Artikel damit an? (auch Unterseiten wie "Prayer/de" müssen berücksichtigt werden)
+            }
+
+            $newBar[$heading][] = [
+                'text'   => $text,
+                'href'   => $href,
+                'id'     => 'n-' . strtr($line[1], ' ', '-'),
+                'active' => $active,
+            ];
         }
         // End Mediawiki source
 
-        if (count($new_bar) > 0) {
-            return $new_bar;
-        } else {
-            return [];
-        }
+        return $newBar;
     }
-
 
     // processes templates and wiki magic words, plus any add'l custom magic words
     private static function customSidebarPreProcess(Skin $skin, $text, Parser $wgParser)
